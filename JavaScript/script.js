b1751b482a287ceb9da8ed49ace3c87a72fb20fc
Soham Datta <sohamdatta1997@gmail.com>
2024-02-25 02:03:26 +0530

    Queries edit and back to top functionalities added

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,25 +1,32 @@
-import { variables } from './vendor.js'
+import { variables } from './vendor.js';
 
+const showBackToTopButton = () => {
+    if(window.scrollY >= 100){
+        variables.backToTopButtonElement.classList.remove('hidden');
+    }else{
+        variables.backToTopButtonElement.classList.add('hidden');
+    }
+}
 const backToTop = () => {
-    window.scrollTo(0, 0);
+    window.scrollTo(0, 0)
+    showBackToTopButton();
 }
 
 const viewResume = () => {
     variables.resumeElement.classList.toggle('hidden');
     variables.backdropOverlay.classList.toggle('hidden');    
 }
-
 const closeViewResume = (element) => {
     if(element.key ==='Escape' && !(variables.resumeElement.classList.contains('hidden'))){
             viewResume();
     }
 }
-
 const mobileNavigator = () => {
     variables.homeHeaderElement.classList.toggle("nav-open");
 }
-
+// Back to top button functionality
 variables.backToTopButtonElement.addEventListener('click', backToTop);
+document.addEventListener('scroll', showBackToTopButton);
 
 //Resume button functionality
 variables.viewMyResumeButtonElement.addEventListener('click', viewResume);
